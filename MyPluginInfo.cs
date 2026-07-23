f4b3e19c6b0d3412cba07e5a7652ec15c0c2f76c
Snack-tacular <42532508+Snack-tacular@users.noreply.github.com>
2026-07-23 21:06:37 +0200

    Update assembly, file, and plugin metadata versions to v1.0.6 to display correctly in in-game mod managers

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace MonsterHPBars
     {
         public const string PLUGIN_GUID    = "com.sineusarena.monsterhpbars";
         public const string PLUGIN_NAME    = "Monster HP Bars";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.6";
     }
 }
